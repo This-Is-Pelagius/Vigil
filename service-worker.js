ad4c7642d3e171ba890a372fac16823b828f7de1
This-Is-Pelagius <pelagius@atomicmail.io>
2026-04-29 07:57:36 +0100

    Update service-worker.js
    Updated 29 Apr 2026; 07h57
    --
    Changed line 7 
    
    const CACHE_VERSION = 'vigil-v2';
    
    to
    
    const CACHE_VERSION = 'vigil-v3';

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
  * When a new version is deployed, increment CACHE_VERSION to bust the cache.
  */
 
-const CACHE_VERSION = 'vigil-v2';
+const CACHE_VERSION = 'vigil-v3';
 
 const APP_SHELL = [
   '/',
