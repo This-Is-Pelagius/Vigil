bfe17e8783471abca75808e53e81d682aed0b8ef
Dominic Parker <dominicsmacbook@Dominics-MacBook-Air.local>
2026-04-29 17:39:55 +0100

    Deploy: 29 Apr 2026 · LIGHT · cache vigil-v3

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,6 @@ const APP_SHELL = [
   '/icons/icon-180.png',
   '/icons/icon-192.png',
   '/icons/icon-512.png',
-  '/icons/icon-256.png',
   '/icons/icon-maskable-192.png',
   '/icons/icon-maskable-512.png'
 ];
