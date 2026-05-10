5df470af383de75a051d37fe80fde756ac3dfc9c
Dominic Parker <pelagius@atomicmail.io>
2026-05-10 20:31:50 +0100

    Deploy: 10 May 2026 · PEACE · cache vigil-v32

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
  * When a new version is deployed, increment CACHE_VERSION to bust the cache.
  */
 
-const CACHE_VERSION = 'vigil-v31';
+const CACHE_VERSION = 'vigil-v32';
 
 const APP_SHELL = [
   '/',
