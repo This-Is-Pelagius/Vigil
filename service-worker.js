fb738a6c01ff85fa7128a76270424a2a0b6cb6d3
Dominic Parker <pelagius@atomicmail.io>
2026-05-15 10:17:43 +0100

    Deploy: 15 May 2026 · TEND · cache vigil-v42

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
  * When a new version is deployed, increment CACHE_VERSION to bust the cache.
  */
 
-const CACHE_VERSION = 'vigil-v41';
+const CACHE_VERSION = 'vigil-v42';
 
 const APP_SHELL = [
   '/',
