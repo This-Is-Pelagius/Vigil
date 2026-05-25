0bb08ef30757b8f613ad953a14d0477f4ab8d41c
Dominic Parker <pelagius@atomicmail.io>
2026-05-25 07:36:30 +0100

    Deploy: 25 May 2026 · MOTHERHOOD · cache vigil-v66

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
  * When a new version is deployed, increment CACHE_VERSION to bust the cache.
  */
 
-const CACHE_VERSION = 'vigil-v65';
+const CACHE_VERSION = 'vigil-v66';
 
 const APP_SHELL = [
   '/',
