fb1ec5e5daca909598b47bdebf8e89b34e2f3567
Dominic Parker <pelagius@atomicmail.io>
2026-05-26 19:43:51 +0100

    Deploy: 26 May 2026 · CHEERFUL · cache vigil-v71

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
  * When a new version is deployed, increment CACHE_VERSION to bust the cache.
  */
 
-const CACHE_VERSION = 'vigil-v70';
+const CACHE_VERSION = 'vigil-v71';
 
 const APP_SHELL = [
   '/',
