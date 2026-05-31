9ec2d0567f31ca095588421e75e73f281639156e
Dominic Parker <pelagius@atomicmail.io>
2026-05-31 22:04:38 +0100

    Add Plausible analytics

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
  * When a new version is deployed, increment CACHE_VERSION to bust the cache.
  */
 
-const CACHE_VERSION = 'vigil-v77';
+const CACHE_VERSION = 'vigil-v78';
 
 const APP_SHELL = [
   '/',
