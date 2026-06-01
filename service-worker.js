a04a1e1f3898e6a13066c6ebe3ed9d4ba98cdeed
Dominic Parker <pelagius@atomicmail.io>
2026-06-01 11:39:51 +0100

    Deploy: 1 Jun 2026 · REASON · cache vigil-v79

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
  * When a new version is deployed, increment CACHE_VERSION to bust the cache.
  */
 
-const CACHE_VERSION = 'vigil-v78';
+const CACHE_VERSION = 'vigil-v79';
 
 const APP_SHELL = [
   '/',
